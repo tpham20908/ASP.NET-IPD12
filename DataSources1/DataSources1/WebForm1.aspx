d6b119af26b1fc2a539aa00b0a7a0896afbdb470
Tung Pham <tungint@gmail.com>
2018-06-07 10:22:06 -0400

    databinding

---
+++ DataSources1/DataSources1/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DataSources1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Dropdown list binding<br />
+            <br />
+            <asp:DropDownList ID="ddlBinding" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddl_SelectedIndexChanged">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Label ID="lblDDLBinding" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DataSources1/DataSources1/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataSources1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.SetFocus(ddlBinding);
+
+            if (!Page.IsPostBack)
+            {
+                // Yes, No, Maybe, OK
+                var answer = new string[] { "Yes", "No", "Maybe", "OK" };
+                // Sourse
+                ddlBinding.DataSource = answer;
+                // Binding
+                ddlBinding.DataBind();
+
+
+            }
+        }
+
+        protected void ddl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblDDLBinding.Text = "Your answer is " + ddlBinding.SelectedValue.ToString();
+        }
+    }
+}
